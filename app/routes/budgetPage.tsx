e5b2075f4c327aa4795e9dbdcf6738b05719551d
mali <mali@cargurus.com>
2023-09-19 16:06:32 -0400

    budget and priorities pages

---
+++ app/routes/budgetPage.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Slider from "@mui/material/Slider";
+import "../styles/budget.css";
+import TextField from "@mui/material/TextField";
+export interface BudgetPageProps {}
+
+function calculateValue(value: number) {
+  return 2 ** value;
+}
+
+export default function NonLinearSlider(props: BudgetPageProps) {
+  const [value, setValue] = React.useState<number>(10);
+  console.log(value);
+  console.log(calculateValue.toLocaleString());
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    if (typeof newValue === "number") {
+      setValue(newValue);
+    }
+  };
+
+  return (
+    <div className="landingContainer">
+      <h1 className="logoHeader">Enter your Budget</h1>
+
+      <Box sx={{ width: 250 }}>
+        <div className="budgetBody">
+          <Typography id="price" gutterBottom>
+            Price: $ {calculateValue(value)}
+          </Typography>
+        </div>
+        <Slider
+          min={5}
+          step={1}
+          max={30}
+          scale={calculateValue}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          aria-labelledby="non-linear-slider"
+        />
+      </Box>
+    </div>
+  );
+}
---
+++ app/routes/priorities.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
+import "../styles/landing.css";
+
+export interface PrioritiesPageProps {}
+
+export default function PrioritiesPage(props: PrioritiesPageProps) {
+  const [value, setValue] = React.useState<number | null>(2);
+
+  return (
+    <div className="landingContainer">
+      <h1 className="logoHeader">RATE YOUR PRIORITIES</h1>
+      <Box
+        sx={{
+          "& > legend": { mt: 2 },
+        }}
+      >
+        <Typography component="legend">SAFETY</Typography>
+        <Rating name="size-large" defaultValue={2} size="large" />
+        <Typography component="legend">PRICE</Typography>
+        <Rating name="size-large" defaultValue={2} size="large" />
+        <Typography component="legend">TECH</Typography>
+        <Rating name="size-large" defaultValue={2} size="large" />
+        <Typography component="legend">UPGRADES</Typography>
+        <Rating name="size-large" defaultValue={2} size="large" />
+      </Box>
+    </div>
+  );
+}
